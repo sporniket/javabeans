783e57d613dd7eaa300abfb19c8aee48026eb534
David SPORN <sporniket.studio@gmail.com>
2017-07-12 07:12:21 +0200

    cleanstep : refactor output, directly use utility classes

--- sporniket-javabeans-core/src/test/java/test/sporniket/libre/javabeans/core/pojo/encapsulator/Encapsulator.java
+++ sporniket-javabeans-core/src/test/java/test/sporniket/libre/javabeans/core/pojo/encapsulator/Encapsulator.java
@@ -7,19 +7,16 @@ import java.io.PrintStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import com.sporniket.libre.javabeans.core.pojo.encapsulator.ClassUtils;
+import com.sporniket.libre.javabeans.core.pojo.encapsulator.FieldUtils;
 
 /**
  * Encapsulate a given Pojo into a Bean.
@@ -43,8 +40,7 @@ public class Encapsulator
 		System.out.println(_toScan.getSimpleName());
 		Arrays.asList(_toScan.getAnnotations()).forEach(a -> System.out.println("  annotation " + a.toString()));
 
-		final List<Field> _fields = Arrays.asList(_toScan.getDeclaredFields());
-		for (final Field _field : _fields.stream().filter(IS_PUBLIC_FIELD).collect(Collectors.toList()))
+		for (final Field _field : FieldUtils.getPublicDeclaredFields(_toScan))
 		{
 			System.out.println("property : " + _field.getName() + " : " + _field.getType().getName());
 			Arrays.asList(_field.getAnnotations()).forEach(a -> System.out.println("  " + "annotation " + a.toString()));
@@ -62,16 +58,6 @@ public class Encapsulator
 	// only one class can be mapped to a shortname
 	final Map<String, String> myShortClassnameMapping = new HashMap<>();
 
-	private String computeFieldAccessorSuffix(Field field)
-	{
-		return field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
-	}
-
-	private String computeOutputClassname(Class<?> classToOutput, Map<String, String> translations, Set<String> shortables)
-	{
-		return ClassUtils.computeOutputClassname(classToOutput, translations, shortables) ;
-	}
-
 	Set<Class<?>> getKnownClasses()
 	{
 		return myKnownClasses;
@@ -82,19 +68,41 @@ public class Encapsulator
 		return myShortClassnameMapping;
 	}
 
-	Map<String, String> getTranslationMapWhenPojosAreSuffixed(Set<Class<?>> registry, Set<String> sourcePackages, String pojoSuffix)
+	/**
+	 * Generate a Javabean defined by encapsulating a pojo.
+	 * 
+	 * @param rawClass
+	 *            the pojo to encapsulate into a javabean.
+	 * @param out
+	 *            the {@link PrintStream} to generate the definition into.
+	 * @param translations
+	 *            a translation table to get the javabeans type from pojo types (since the goal is to convert a set of related pojo
+	 *            into a set of related javabeans).
+	 * @param shortables
+	 *            the list of classes that can be shortened (use the simple name instead of the full classname after the import
+	 *            declaration)
+	 */
+	private void outputJavabean(Class<?> rawClass, PrintStream out, final Map<String, String> translations,
+			final Set<String> shortables)
 	{
-		return ClassUtils.getTranslationMapWhenPojosAreSuffixed(registry, sourcePackages, pojoSuffix);
+		outputJavabean__classBegin(rawClass, out, translations, shortables);
+
+		FieldUtils.getPublicDeclaredFields(rawClass).forEach(_field -> {
+			outputJavabean__property(_field, out, translations, shortables);
+		});
+
+		outputJavabean__classEnd(rawClass, out, translations, shortables);
 	}
 
-	private void outputJavabeanClassDeclaration(Class<?> toScan, PrintStream _out, Map<String, String> _translation,
+	private void outputJavabean__classBegin(Class<?> toScan, PrintStream _out, Map<String, String> _translation,
 			Set<String> _shortables)
 	{
 		final StringBuilder _classDecl = new StringBuilder("public class ");
-		_classDecl.append(computeOutputClassname(toScan, _translation, _shortables));
+		_classDecl.append(ClassUtils.computeOutputClassname(toScan, _translation, _shortables));
 		if (!Object.class.equals(toScan.getSuperclass()))
 		{
-			_classDecl.append(" extends ").append(computeOutputClassname(toScan.getSuperclass(), _translation, _shortables));
+			_classDecl.append(" extends ")
+					.append(ClassUtils.computeOutputClassname(toScan.getSuperclass(), _translation, _shortables));
 		}
 		final Class<?>[] _interfaces = toScan.getInterfaces();
 		if (_interfaces.length > 0)
@@ -102,7 +110,7 @@ public class Encapsulator
 			for (int _i = 0; _i < _interfaces.length; _i++)
 			{
 				_classDecl.append((0 == _i) ? " implements " : ", ")
-						.append(computeOutputClassname(_interfaces[_i], _translation, _shortables));
+						.append(ClassUtils.computeOutputClassname(_interfaces[_i], _translation, _shortables));
 			}
 		}
 
@@ -115,17 +123,34 @@ public class Encapsulator
 		_out.println();
 	}
 
+	private void outputJavabean__classEnd(Class<?> toScan, PrintStream _out, Map<String, String> _translation,
+			Set<String> _shortables)
+	{
+		_out.println("}\n");
+	}
+
+	private void outputJavabean__property(final Field field, PrintStream out, final Map<String, String> translation,
+			final Set<String> shortables)
+	{
+		final String _accessorSuffix = FieldUtils.computeFieldAccessorSuffix(field);
+		final String _type = ClassUtils.computeOutputClassname(field.getType(), translation, shortables);
+
+		// getter
+		out.printf("    public %s get%s() {return pojo.%s ;}\n", _type, _accessorSuffix, field.getName());
+
+		// setter
+		out.printf("    public void set%s(%s value) {pojo.%s = value;}\n", _accessorSuffix, _type, field.getName());
+
+		out.println();
+	}
+
 	private void processPojoClass(Class<?> toScan, PrintStream _out)
 	{
 		_out.printf("package %s;\n", toScan.getPackage().getName());
 
 		_out.println();
 
-		// register superclass and interfaces
-		final List<Field> _beanFields = Arrays.asList(toScan.getDeclaredFields()).stream().filter(IS_PUBLIC_FIELD)
-				.collect(Collectors.toList());
-
-		registerKnownClasses(toScan, _beanFields);
+		ClassUtils.updateKnownClasses(getKnownClasses(), toScan);
 
 		final Predicate<? super Class<?>> _isNotJavaLangType = c -> !Object.class.getPackage().equals(c.getPackage());
 		final Predicate<? super Class<?>> _isNotInSamePackage = c -> !toScan.getPackage().equals(c.getPackage());
@@ -135,7 +160,7 @@ public class Encapsulator
 
 		_out.println();
 
-		final Map<String, String> _translation = getTranslationMapWhenPojosAreSuffixed(getKnownClasses(),
+		final Map<String, String> _translation = ClassUtils.getTranslationMapWhenPojosAreSuffixed(getKnownClasses(),
 				Arrays.asList(toScan.getPackage().getName()).stream().collect(Collectors.toCollection(TreeSet::new)), "Raw");
 		if (_translation.isEmpty())
 		{
@@ -149,9 +174,9 @@ public class Encapsulator
 
 		_out.println();
 
-		registerShortClassnameMapping(getKnownClasses());
 		// update shortnames mapping
-		registerShortClassnameMappingFromString(_translation.values());
+		ClassUtils.updateShortClassnameMappingFromClasses(getShortClassnameMapping(), getKnownClasses());
+		ClassUtils.updateShortClassnameMappingFromStrings(getShortClassnameMapping(), _translation.values());
 
 		final Map<String, String> _shortNameMapping = getShortClassnameMapping();
 		if (_shortNameMapping.isEmpty())
@@ -171,39 +196,7 @@ public class Encapsulator
 
 		// ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ====
 		// generate class
-		outputJavabeanClassDeclaration(toScan, _out, _translation, _shortables);
-
-		for (final Field _field : _beanFields)
-		{
-			final String _accessorSuffix = computeFieldAccessorSuffix(_field);
-			final String _type = computeOutputClassname(_field.getType(), _translation, _shortables);
-
-			// getter
-			_out.printf("    public %s get%s() {return pojo.%s ;}\n", _type, _accessorSuffix, _field.getName());
-
-			// setter
-			_out.printf("    public void set%s(%s value) {pojo.%s = value;}\n", _accessorSuffix, _type, _field.getName());
-
-			_out.println();
-		}
-
-		_out.println("}\n");
+		outputJavabean(toScan, _out, _translation, _shortables);
 
 	}
-
-	private void registerKnownClasses(Class<?> toScan, List<Field> beanFields)
-	{
-		ClassUtils.updateKnownClasses(getKnownClasses(), toScan);
-	}
-
-	private void registerShortClassnameMapping(Set<Class<?>> registry)
-	{
-		ClassUtils.updateShortClassnameMappingFromClasses(getShortClassnameMapping(), registry);
-	}
-
-	private void registerShortClassnameMappingFromString(Collection<String> registry)
-	{
-		ClassUtils.updateShortClassnameMappingFromStrings(getShortClassnameMapping(), registry);
-	}
-
 }
