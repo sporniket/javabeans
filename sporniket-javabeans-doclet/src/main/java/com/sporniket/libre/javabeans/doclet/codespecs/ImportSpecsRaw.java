f2b74f8bce33b436bf0681f60b6e93092df633f6
David SPORN <sporniket.studio@gmail.com>
2017-11-09 06:48:05 +0100

    prepare code generation, model of ImportSpecs

---
+++ sporniket-javabeans-doclet/src/main/java/com/sporniket/libre/javabeans/doclet/codespecs/ImportSpecsRaw.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.sporniket.libre.javabeans.doclet.codespecs;
+
+/**
+ * @author dsporn
+ *
+ */
+class ImportSpecsRaw
+{
+	String className ;
+	Boolean directlyRequired ;
+}
---
+++ sporniket-javabeans-doclet/src/main/java/com/sporniket/libre/javabeans/doclet/codespecs/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Model of data to inject in a code template.
+ * 
+ * @author dsporn
+ *
+ */
+package com.sporniket.libre.javabeans.doclet.codespecs;
