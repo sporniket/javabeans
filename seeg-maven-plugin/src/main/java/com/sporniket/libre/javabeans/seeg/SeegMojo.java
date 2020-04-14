21c0152cd6d4b6a1b9556d81ce14b5ca7d7126d5
David SPORN <sporniket.studio@gmail.com>
2020-04-14 23:58:37 +0200

    connect mojo to seeg

--- seeg-maven-plugin/src/main/java/com/sporniket/libre/javabeans/seeg/SeegMojo.java
+++ seeg-maven-plugin/src/main/java/com/sporniket/libre/javabeans/seeg/SeegMojo.java
@@ -1,66 +1,104 @@
 package com.sporniket.libre.javabeans.seeg;
 
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
+import static org.apache.commons.lang3.StringUtils.trimToEmpty;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 /**
  * Goal which touches a timestamp file.
  */
-@Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
-public class SeegMojo
-    extends AbstractMojo
+@Mojo(name = "seeg")
+public class SeegMojo extends AbstractMojo
 {
-    /**
-     * Location of the file.
-     */
-    @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
-    private File outputDirectory;
+	/**
+	 * Path to the Java source directory of the target project.
+	 */
+	@Parameter(defaultValue = "${project.build.sourceDirectory}", property = "sourceDir", required = true, readonly = true)
+	private File sourceDirectory;
 
-    public void execute()
-        throws MojoExecutionException
-    {
-        File f = outputDirectory;
+	@Parameter(defaultValue = "${project.basedir}/seeg-connection.properties", property = "connectionConfig", required = true)
+	private File connectionConfiguration;
 
-        if ( !f.exists() )
-        {
-            f.mkdirs();
-        }
+	@Parameter(property = "targetPackage", required = true)
+	private String targetPackage;
 
-        File touch = new File( f, "touch.txt" );
+	@Parameter(property = "schemaName", required = false)
+	private String schemaName;
 
-        FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
+	@Override
+	public void execute() throws MojoExecutionException
+	{
+		if (StringUtils.isBlank(targetPackage))
+		{
+			throw new MojoExecutionException("Target package MUST be specified.");
+		}
+		final String _localPath = trimToEmpty(targetPackage).replace('.', File.separatorChar);
+		String _targetDirectory = new File(sourceDirectory, _localPath).getAbsolutePath();
+		String _connectionConfiguration = connectionConfiguration.getAbsolutePath();
+		String _schemaName = StringUtils.trimToNull(schemaName);
 
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
-                    w.close();
-                }
-                catch ( IOException e )
-                {
-                    // ignore
-                }
-            }
-        }
-    }
+		try (PipedInputStream pis = new PipedInputStream();
+				PipedOutputStream pos = new PipedOutputStream(pis);
+				BufferedReader br = new BufferedReader(new InputStreamReader(pis), 32000);
+				PrintStream out = new PrintStream(pos);)
+		{
+			final List<Throwable> errorStack = new ArrayList<Throwable>(1);
+			final ExecutorService executor = newSingleThreadExecutor();
+			executor.submit(() -> {
+				try
+				{
+					new Seeg().perform(_connectionConfiguration, _targetDirectory, targetPackage, _schemaName, out);
+				}
+				catch (Exception _error)
+				{
+					// _error.printStackTrace(out);
+					errorStack.add(_error);
+				}
+				finally
+				{
+					executor.shutdown();
+				}
+			});
+
+			while (!executor.isShutdown())
+			{
+				try
+				{
+					String nextLine = br.readLine();
+					if (null != nextLine)
+					{
+						getLog().info(nextLine);
+					}
+				}
+				catch (IOException _error)
+				{
+					getLog().error("Error while reading pipe.", _error);
+				}
+			}
+			if (!errorStack.isEmpty())
+			{
+				throw errorStack.get(0);
+			}
+		}
+		catch (Throwable _error)
+		{
+			throw new MojoExecutionException("A problem occurred.", _error);
+		}
+	}
 }
