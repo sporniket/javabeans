2acd815cfb767267f25721b21df83b9335f16920
David SPORN <sporniket.studio@gmail.com>
2020-04-15 22:50:52 +0200

    javadoc : class doc

--- seeg-maven-plugin/src/main/java/com/sporniket/libre/javabeans/seeg/Seeg.java
+++ seeg-maven-plugin/src/main/java/com/sporniket/libre/javabeans/seeg/Seeg.java
@@ -1,10 +1,10 @@
 package com.sporniket.libre.javabeans.seeg;
 
+import static com.sporniket.libre.javabeans.seeg.CodeGeneratorHelper.FROM_DEF_ENUM;
 import static com.sporniket.libre.javabeans.seeg.CodeGeneratorHelper.FROM_DEF_TABLE_TO_ENTITY;
 import static com.sporniket.libre.javabeans.seeg.CodeGeneratorHelper.FROM_DEF_TABLE_TO_FINDER;
 import static com.sporniket.libre.javabeans.seeg.CodeGeneratorHelper.FROM_DEF_TABLE_TO_ID_CLASS;
 import static com.sporniket.libre.javabeans.seeg.CodeGeneratorHelper.FROM_DEF_TABLE_TO_REPOSITORY;
-import static com.sporniket.libre.javabeans.seeg.CodeGeneratorHelper.FROM_DEF_ENUM;
 import static com.sporniket.libre.javabeans.seeg.ConsoleHelper.printHeader;
 import static java.lang.String.format;
 
@@ -23,9 +23,34 @@ import java.util.Properties;
  * Utility program that connects to a database, and generate Java JPA entities and Spring-data-jpa repositories from the tables of a
  * schema.
  * </p>
- * 
- * @author dsporn
  *
+ * <p>
+ * &copy; Copyright 2012-2020 David Sporn
+ * </p>
+ * <hr>
+ *
+ * <p>
+ * This file is part of <i>The Sporniket Javabeans Project &#8211; doclet</i>.
+ *
+ * <p>
+ * <i>The Sporniket Javabeans Project &#8211; seeg</i> is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * <p>
+ * <i>The Sporniket Javabeans Project &#8211; seeg</i> is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License along with <i>The Sporniket Javabeans Library &#8211;
+ * core</i>. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. 2
+ *
+ * <hr>
+ *
+ * @author David SPORN
+ * @version 20.04.01
+ * @since 20.04.01
  */
 public class Seeg
 {
