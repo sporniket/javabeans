6a254436176563a8efd778448078fbee482ea77d
SPORN David (Consulting for ACCOR Hotels) <david.sporn@consulting-for.accor.com>
2025-08-28 09:07:07 +0200

    extract codespecs package into own library

---
+++ sporniket-javabeans-models/src/main/java/com/sporniket/libre/javabeans/models/javacode/ClassSpecs.java
@@ -0,0 +1,105 @@
+package com.sporniket.libre.javabeans.models.javacode;
+
+import java.util.Collection;
+import java.util.List;
+
+/** GENERATED CODE !
+ * <p>
+ * &copy; Copyright 2012-2025 David Sporn
+ * </p>
+ * <hr>
+ *
+ * <p>
+ * This file is part of <i>The Sporniket Javabeans Project &#8211; doclet</i>.
+ *
+ * <p>
+ * <i>The Sporniket Javabeans Project &#8211; doclet</i> is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * <p>
+ * <i>The Sporniket Javabeans Project &#8211; doclet</i> is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License along with <i>The Sporniket Javabeans Library &#8211;
+ * core</i>. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. 2
+ *
+ * <hr>
+ *
+ * @author David SPORN
+ * @version 25.11.00
+ * @since 25.11.00
+ */
+public class ClassSpecs 
+{
+
+    private boolean myAbstractRequired ;
+
+    private List<AnnotationSpecs> myAnnotations ;
+
+    private String myClassName ;
+
+    private String myClassNameFullyQualified ;
+
+    private String myClassNameOutput ;
+
+    private String myDeclaredTypeArguments ;
+
+    private List<FieldSpecs> myFields ;
+
+    private Collection<ImportSpecs> myImports ;
+
+    private String myInterfaceList ;
+
+    private String myInvokedTypeArguments ;
+
+    private String[] myJavadocLines ;
+
+    private String myPackageName ;
+
+    private String mySuperClassName ;
+
+
+    public boolean isAbstractRequired() {return myAbstractRequired ;}
+    public void setAbstractRequired(boolean value) {myAbstractRequired = value;}
+
+    public List<AnnotationSpecs> getAnnotations() {return myAnnotations ;}
+    public void setAnnotations(List<AnnotationSpecs> value) {myAnnotations = value;}
+
+    public String getClassName() {return myClassName ;}
+    public void setClassName(String value) {myClassName = value;}
+
+    public String getClassNameFullyQualified() {return myClassNameFullyQualified ;}
+    public void setClassNameFullyQualified(String value) {myClassNameFullyQualified = value;}
+
+    public String getClassNameOutput() {return myClassNameOutput ;}
+    public void setClassNameOutput(String value) {myClassNameOutput = value;}
+
+    public String getDeclaredTypeArguments() {return myDeclaredTypeArguments ;}
+    public void setDeclaredTypeArguments(String value) {myDeclaredTypeArguments = value;}
+
+    public List<FieldSpecs> getFields() {return myFields ;}
+    public void setFields(List<FieldSpecs> value) {myFields = value;}
+
+    public Collection<ImportSpecs> getImports() {return myImports ;}
+    public void setImports(Collection<ImportSpecs> value) {myImports = value;}
+
+    public String getInterfaceList() {return myInterfaceList ;}
+    public void setInterfaceList(String value) {myInterfaceList = value;}
+
+    public String getInvokedTypeArguments() {return myInvokedTypeArguments ;}
+    public void setInvokedTypeArguments(String value) {myInvokedTypeArguments = value;}
+
+    public String[] getJavadocLines() {return myJavadocLines ;}
+    public void setJavadocLines(String[] value) {myJavadocLines = value;}
+
+    public String getPackageName() {return myPackageName ;}
+    public void setPackageName(String value) {myPackageName = value;}
+
+    public String getSuperClassName() {return mySuperClassName ;}
+    public void setSuperClassName(String value) {mySuperClassName = value;}
+
+}
+
---
+++ sporniket-javabeans-models/src/main/java/com/sporniket/libre/javabeans/models/javacode/Comparators.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.sporniket.libre.javabeans.models.javacode;
+
+import static com.sporniket.strings.StringComparators.STRING_COMPARATOR_NATURAL;
+import static com.sporniket.strings.StringComparators.STRING_COMPARATOR_REVERSE;
+import static java.util.Comparator.nullsFirst;
+
+import java.util.Comparator;
+
+/**
+ * Builtin comparators for the model.
+ * <p>
+ * &copy; Copyright 2012-2023 David Sporn
+ * </p>
+ * <hr>
+ *
+ * <p>
+ * This file is part of <i>The Sporniket Javabeans Project &#8211; doclet</i>.
+ *
+ * <p>
+ * <i>The Sporniket Javabeans Project &#8211; doclet</i> is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * <p>
+ * <i>The Sporniket Javabeans Project &#8211; doclet</i> is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License along with <i>The Sporniket Javabeans Library &#8211;
+ * core</i>. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. 2
+ *
+ * <hr>
+ *
+ * @author David SPORN
+ * @version 25.11.00
+ * @since 19.02.00
+ */
+public final class Comparators
+{
+	public final static Comparator<ImportSpecs> IMPORT_SPECS_COMPARATOR_NATURAL = nullsFirst(
+			(is1, is2) -> STRING_COMPARATOR_NATURAL.compare(is1.getClassName(), is2.getClassName()));//
+
+	public final static Comparator<ImportSpecs> IMPORT_SPECS_COMPARATOR_REVERSE = nullsFirst(
+			(is1, is2) -> STRING_COMPARATOR_REVERSE.compare(is1.getClassName(), is2.getClassName()));//
+}
